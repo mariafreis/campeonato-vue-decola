b51e67c67e12e1ac8735403651d7b8af33d0803e
Maria Fernanda <ferdinandamoge@gmail.com>
2022-03-16 21:20:35 -0300

    Antes de instalar o router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <AppTopBar />
+
+    <v-main>
+      <MeuInicio />
+    </v-main>
+  </v-app>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MeuInicio from "./components/MeuInicio";
+import AppTopBar from "./components/shared/AppTopBar";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    MeuInicio,
+    AppTopBar,
+  },
+};
+</script>
